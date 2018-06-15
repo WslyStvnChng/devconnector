dd7db68bcff72188cdb9797eaa975649ceccb3bd
WslyStvnChng <mr.cheng.st@gmail.com^C>
2018-06-15 00:33:05 -0600

    added routes/api/users to encrypt password, add user, email and gravatar. Also use brcypt to hash password

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,11 @@
 
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+// Load User Model
+const User = require("../../models/User");
 
 // @route         GET api/users/test
 // @description   Test Users route
@@ -9,5 +14,49 @@ const router = express.Router();
 router.get('/test', (req, res) => res.json({
   msg: "Users Works"
 }));
+
+// @route         GET api/users/register
+// @description   Register user
+// @access        Public
+router.post('/register', (req, res) => {
+  User.findOne({
+      email: req.body.email
+    })
+    .then(user => {
+      // If there is an user with that email address
+      if (user) {
+        return res.status(400).json({
+          email: 'Email already exists'
+        });
+        // Else use gravatar from schema email
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: '200', // Size
+          r: 'pg', // Rating
+          d: 'mm' // Default
+        });
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar: avatar,
+          password: req.body.password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) {
+              throw err;
+            } else {
+              newUser.password = hash;
+              newUser.save()
+                .then(user => res.json(user))
+                .catch(err => console.log(err));
+            }
+          });
+        });
+      }
+    });
+});
+
 
 module.exports = router;
